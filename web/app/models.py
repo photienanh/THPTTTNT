693ac3a8b2295d1c12a21c3a4878b7a5592b4c88
NguyenPhuongDng <144362042+NguyenPhuongDng@users.noreply.github.com>
2024-05-23 16:10:44 +0700

    hmm

--- web/app/models.py
+++ web/app/models.py
@@ -1,68 +1,76 @@
 from django.db import models
 
-# Create your models here.
 class User(models.Model):
-    ID = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
-    email = models.EmailField(max_length=200)
-    phone = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200, unique=True)
+    phone = models.CharField(max_length=200, unique=True)
     password = models.CharField(max_length=200)
-    username = models.CharField(max_length=200)
+    username = models.CharField(max_length=200, unique=True)
     role = models.CharField(max_length=200)
+
 class Promo(models.Model):
     name = models.CharField(max_length=200)
     value = models.CharField(max_length=200)
+
 class Detail(models.Model):
     screen = models.CharField(max_length=200)
     os = models.CharField(max_length=200)
-    camara = models.CharField(max_length=200)
-    camaraFront = models.CharField(max_length=200)
+    camera = models.CharField(max_length=200)
+    camera_front = models.CharField(max_length=200)
     cpu = models.CharField(max_length=200)
     ram = models.CharField(max_length=200)
     rom = models.CharField(max_length=200)
-    microUSB = models.CharField(max_length=200)
+    micro_usb = models.CharField(max_length=200)
     battery = models.CharField(max_length=200)
 
 class Product(models.Model):
+    id = models.CharField(primary_key=True, max_length=200)
     name = models.CharField(max_length=255)
     company = models.CharField(max_length=255)
     img = models.CharField(max_length=255)
     price = models.CharField(max_length=255)
     star = models.IntegerField(default=0)
-    rateCount = models.IntegerField(default=0)
+    rate_count = models.IntegerField(default=0)
     promo = models.OneToOneField(Promo, on_delete=models.CASCADE)
     detail = models.OneToOneField(Detail, on_delete=models.CASCADE)
+
 class Payment(models.Model):
-    ID = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     date_payment = models.DateField()
     method_payment = models.CharField(max_length=200)
+
 class ShopCart(models.Model):
-    ID = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
 
-class ShopOder(models.Model):
-    ID = models.AutoField(primary_key=True)
-    dateorder = models.DateField()
-    totalprice = models.CharField(max_length=200)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
-    payment_id = models.ForeignKey(Payment, on_delete=models.CASCADE)
-    satatus_oder = models.CharField(max_length=200)
-    shippingid = models.CharField(max_length=200)
+class ShopOrder(models.Model):
+    id = models.AutoField(primary_key=True)
+    date_order = models.DateField()
+    total_price = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    payment = models.ForeignKey(Payment, on_delete=models.CASCADE)
+    status_order = models.CharField(max_length=200)
+    shipping = models.ForeignKey('Shipping', on_delete=models.CASCADE)
 
 class UserAccount(models.Model):
     username = models.CharField(max_length=200)
     password = models.CharField(max_length=200)
-    account_id = models.AutoField(primary_key=True)
+    account_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class UserComment(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     comment = models.CharField(max_length=200)
     post_id = models.AutoField(primary_key=True)
-    
-    
+
+class Shipping(models.Model):
+    id = models.CharField(primary_key=True, max_length=200)
+    kind = models.CharField(max_length=200)
+    company = models.CharField(max_length=200)
+    price = models.FloatField()
