883bd727a153cbf9df91d155698cbd12554d7765
Duy Hau <duyhautd13@gmail.com>
2024-05-23 15:21:22 +0700

    changes

--- web/app/admin.py
+++ web/app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User,Promo,Detail,Product,Payment,ShopCart,ShopOder,UserAccount,UserComment
+admin.site.register(User)
+admin.site.register(Promo)
+admin.site.register(Detail)
+admin.site.register(Product)
+admin.site.register(Payment)
+admin.site.register(ShopCart)
+admin.site.register(ShopOder)
+admin.site.register(UserAccount)
+admin.site.register(UserComment)
